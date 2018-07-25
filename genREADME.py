d46dc4e8750014248b64aae03bbe17dc4df53fa3
Cartor <cartor@gmail.com>
2018-07-25 16:56:16 +0800

    to update README
    - to add sort into genREADME.
    - to update New MacBook Pro list.

--- genREADME.py
+++ genREADME.py
@@ -1,4 +1,5 @@
 import datetime, json, os
+import settings
 
 def writeFile(filename, data):
 	with open(filename, 'a') as outfile:
@@ -10,21 +11,7 @@ def readJson(filename):
 		return data
 
 def main():
-	MacInfoURL = {
-		#"iPod touch": "https://support.apple.com/en-us/HT204217",
-		#"iPad": "https://support.apple.com/en-us/HT201471",
-		#"Apple Watch": "https://support.apple.com/en-us/HT204507",
-		#"Apple TV": "https://support.apple.com/en-us/HT200008",
-		"MacBook Pro": "https://support.apple.com/en-us/HT201300",
-		"MacBook Air": "https://support.apple.com/en-us/HT201862",
-		"MacBook": "https://support.apple.com/en-us/HT201608",
-		"iMac": "https://support.apple.com/en-us/HT201634",
-		"Mac mini": "https://support.apple.com/en-us/HT201894",
-		"Mac Pro": "https://support.apple.com/en-us/HT202888"
-	}
-
 	readmeFileName = "README.md"
-
 	os.remove(readmeFileName)
 
 	MDData = "# AppleDeviceWithIdentifierName\n\
@@ -33,16 +20,17 @@ Apple device's name map model Identifier (Mac)\n\
 "
 	writeFile(readmeFileName, MDData)
 
-	for attr, value in MacInfoURL.iteritems():
+	for attr, value in settings.MacInfoURL.iteritems():
 		print attr, value
 		jsonFileName = "json/" + attr.replace(' ', '_') + ".json"
 
 		writeFile(readmeFileName, "\n## [" + attr + "](" +jsonFileName+ ")\n\n")
-		writeFile(readmeFileName, "> Tech Specs: " + value + "\n\n")
+		writeFile(readmeFileName, "> Tech Specs: " + value[0] + "\n\n")
 		writeFile(readmeFileName, "| Identifier | Device Name | \n| ---------- | ----------- |\n")
 
 		jsonData = readJson(jsonFileName)
-		for identifier, deviceName in jsonData.iteritems():
+
+		for identifier, deviceName in sorted(jsonData.iteritems(), key=lambda (k,v): (k,v)):
 			writeFile(readmeFileName, "| " +identifier+ " | "+deviceName+"  |\n")
 
 		writeFile(readmeFileName, "\n")
